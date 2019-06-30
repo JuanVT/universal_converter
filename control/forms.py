ed0034b201501cb99091f0e287a2d8fb0447bfef
Juan Vera <juanjoseveratorres@gmail.com>
2019-06-30 13:42:46 +0100

    Converting form into Django Form

--- control/forms.py
+++ control/forms.py
@@ -1,10 +1,30 @@
-from django.forms import ModelForm
-
-from control.models import Converter
+from django import forms
 
 
-class ConverterForm(ModelForm):
+# from control.models import Converter
+#
+#
+# class ConverterForm(ModelForm):
+#
+#     class Meta:
+#         model = Converter
+#         fields = ['unit', 'unit_value', 'unit_to', ]
+from django.utils.regex_helper import Choice
 
-    class Meta:
-        model = Converter
-        fields = ['unit', 'unit_value', 'unit_to', ]
+unit_choices = [("metre", "Metre"),
+                ("kilometre", "Kilometre"),
+                ("centimetre", "Centimetre"),
+                ("millimetre", "Millimetre"),
+                ("micrometre", "Micrometre"),
+                ("nanometre", "Nanometre"),
+                ("mile", "Mile"),
+                ("yard", "Yard"),
+                ("foot", "Foot"),
+                ("inch", "Inch")]
+
+
+class ConverterForm(forms.Form):
+
+    unit = forms.ChoiceField(label='Unit', choices='unit_choices', initial='', widget=forms.Select(), required=True)
+    unit_to = forms.ChoiceField(label='Unit to', choices='unit_choices', initial='', widget=forms.Select(), required=True)
+    unit_value = forms.FloatField()
--- control/models.py
+++ control/models.py
@@ -1,19 +1,19 @@
-from django.db import models
-
-unit_choices = [("metre", "Metre"),
-                ("kilometre", "Kilometre"),
-                ("centimetre", "Centimetre"),
-                ("millimetre", "Millimetre"),
-                ("micrometre", "Micrometre"),
-                ("nanometre", "Nanometre"),
-                ("mile", "Mile"),
-                ("yard", "Yard"),
-                ("foot", "Foot"),
-                ("inch", "Inch")]
-
-
-class Converter(models.Model):
-
-    unit = models.CharField(null=True, blank=False, max_length=15, choices=unit_choices)
-    unit_to = models.CharField(null=True, blank=False, max_length=15, choices=unit_choices)
-    unit_value = models.FloatField(null=True, blank=False)
+# from django.db import models
+#
+# unit_choices = [("metre", "Metre"),
+#                 ("kilometre", "Kilometre"),
+#                 ("centimetre", "Centimetre"),
+#                 ("millimetre", "Millimetre"),
+#                 ("micrometre", "Micrometre"),
+#                 ("nanometre", "Nanometre"),
+#                 ("mile", "Mile"),
+#                 ("yard", "Yard"),
+#                 ("foot", "Foot"),
+#                 ("inch", "Inch")]
+#
+#
+# class Converter(models.Model):
+#
+#     unit = models.CharField(null=True, blank=False, max_length=15, choices=unit_choices)
+#     unit_to = models.CharField(null=True, blank=False, max_length=15, choices=unit_choices)
+#     unit_value = models.FloatField(null=True, blank=False)
