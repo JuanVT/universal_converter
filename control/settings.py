766dcd6ae259f464ae857c22f8c49e8784f4b3a0
JuanVT <juanjoseveratorres@gmail.com>
2019-07-14 20:31:13 +0100

    Deleting staticfiles_dir

--- control/settings.py
+++ control/settings.py
@@ -124,9 +124,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "control/static"),
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
