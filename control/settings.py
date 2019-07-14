edf611aaf2c2da01969ad620429a292aaf9bcf98
JuanVT <juanjoseveratorres@gmail.com>
2019-07-14 18:42:37 +0100

    Modifiying Static_Dirs

--- control/settings.py
+++ control/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', 'control/static'),
+    os.path.join(BASE_DIR, 'control', 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
