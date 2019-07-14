f2f37f8c1eede1d532884af084300a1b302563e9
JuanVT <juanjoseveratorres@gmail.com>
2019-07-14 22:19:16 +0100

    Changing Settings

--- control/settings.py
+++ control/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
     os.path.join(APP_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
